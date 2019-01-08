2c466aa67d8fb9eaa1b9cfde975d36b084dc86f6
Leon Hassan <leonhassan@hotmail.com>
2019-01-08 11:18:54 +0000

    Updates

---
+++ precache-manifest.23d772369e8a4f83bbc6fd0a46b1fa78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2a5bdf8ccc3752b3de3",
+    "url": "/leonhassan.co.uk/static/css/main.6daf75a8.chunk.css"
+  },
+  {
+    "revision": "b2a5bdf8ccc3752b3de3",
+    "url": "/leonhassan.co.uk/static/js/main.b2a5bdf8.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/leonhassan.co.uk/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "0e3ffe3386b8c57bc8ef",
+    "url": "/leonhassan.co.uk/static/js/runtime~main.0e3ffe33.js"
+  },
+  {
+    "revision": "447fbb9eb3b35a3abbf773a9ee97c954",
+    "url": "/leonhassan.co.uk/index.html"
+  }
+];
