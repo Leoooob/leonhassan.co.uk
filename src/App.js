27f159bb8aad05e1e9f67250182ef6ed30e20847
Leon Hassan <leonhassan@hotmail.com>
2019-03-16 20:26:33 +0000

    added print-only text to blog link in header

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ class ContactGlyphs extends Header {
         </span>
         <span>
           <a href="https://blog.leonhassan.co.uk/" rel="noopener noreferrer" target="_blank">
-            Blog
+            Blog<span className="print-only-view">.leonhassan.co.uk</span>
           </a>
         </span>
         <span>
