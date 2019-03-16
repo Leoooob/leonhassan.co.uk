cef2b9cf4b6542071178ef5e53b04197b0ae774d
Leon Hassan <leonhassan@hotmail.com>
2019-03-16 20:14:43 +0000

    Updates

--- precache-manifest.371d539cbcd172721f2f0200a101a9ec.js
+++ precache-manifest.371d539cbcd172721f2f0200a101a9ec.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/leonhassan.co.uk/static/js/runtime~main.0e3ffe33.js"
   },
   {
-    "revision": "d3f4c5524fdc0d0d8d3e",
-    "url": "/leonhassan.co.uk/static/js/main.d3f4c552.chunk.js"
+    "revision": "642db2d366468c653774",
+    "url": "/leonhassan.co.uk/static/js/main.642db2d3.chunk.js"
   },
   {
     "revision": "39ef1c471419a3da2b8f",
     "url": "/leonhassan.co.uk/static/js/1.39ef1c47.chunk.js"
   },
   {
-    "revision": "d3f4c5524fdc0d0d8d3e",
-    "url": "/leonhassan.co.uk/static/css/main.39cc1ad1.chunk.css"
+    "revision": "642db2d366468c653774",
+    "url": "/leonhassan.co.uk/static/css/main.59b52304.chunk.css"
   },
   {
-    "revision": "5973a2c88d00825d3869293683a64f27",
+    "revision": "6b9d39e77a3907739735cc2ea88d23bd",
     "url": "/leonhassan.co.uk/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/leonhassan.co.uk/precache-manifest.0e13be0c9e43929290dd1b7cac658092.js"
+  "/leonhassan.co.uk/precache-manifest.371d539cbcd172721f2f0200a101a9ec.js"
 );
 
 workbox.clientsClaim();
