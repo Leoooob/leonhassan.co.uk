04fd9d19d918c2a32ea5c850ed0c568a8d09d111
Leon Hassan <leonhassan@hotmail.com>
2019-04-03 10:31:57 +0530

    updated class statements to use classname

--- src/App.js
+++ src/App.js
@@ -246,8 +246,8 @@ class ReadMoreLink extends Content {
     return (
       <div className="read-more">
         <a href={this._getLink()} rel="noopener noreferrer" target="_blank">
-          <span class="web-text">{`Read more about my experience at ${this.props.company} here`}</span>
-          <span class="print-only-view">{`Read more at ${this._getLink()}`}</span>
+          <span className="web-text">{`Read more about my experience at ${this.props.company} here`}</span>
+          <span className="print-only-view">{`Read more at ${this._getLink()}`}</span>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon"><path className="icon-primary" d="M12 8a1 1 0 0 1-1 1H5v10h10v-6a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9c0-1.1.9-2 2-2h6a1 1 0 0 1 1 1z"/><path className="icon-secondary" d="M19 6.41L8.7 16.71a1 1 0 1 1-1.4-1.42L17.58 5H14a1 1 0 0 1 0-2h6a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V6.41z"/></svg>
         </a>
       </div>
