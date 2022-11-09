ed9de287a31aaf4d0989cb6a6676f1a4c492365a
Leon Hassan <leonhassan@hotmail.com>
2022-11-09 17:23:33 +0000

    updated geographical location

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class Aside extends React.Component {
       <aside>
         <div className="location">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="icon"><path className="icon-primary" d="M5.64 16.36a9 9 0 1 1 12.72 0l-5.65 5.66a1 1 0 0 1-1.42 0l-5.65-5.66zM12 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/><path className="icon-secondary" d="M12 1a9 9 0 0 1 6.36 15.36l-5.65 5.66a1 1 0 0 1-.71.3V13a3 3 0 0 0 0-6V1z"/></svg>
-          <span>Southampton, UK</span>
+          <span>Reading, UK</span>
         </div>
         <h2>Skills Wall</h2>
         <SkillWall/>
